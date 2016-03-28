8b223e91f38b3845313e7da8245224d30622b196
Samuel Mueller <mueller.samu@gmail.com>
2016-03-28 15:07:10 +0200

    v2

--- main.elm
+++ main.elm
@@ -2,17 +2,22 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Graphics.Element exposing (..)
+-- import Graphics.Input exposing (..)
 import Signal exposing (..)
+
+import Graphics.Input exposing (dropDown)
 
 type alias Model =
   { field : String
   , entries : List Snippet
   , currentIndex : Int
+  , currentSnippetType : SnippetType
   }
 
 type alias Snippet =
   { content : String
   , index : Int
+  , snippetType : SnippetType
   }
 
 type Action
@@ -20,9 +25,17 @@ type Action
   | UpdateField String
   | AddEntry
   | DeleteEntry Int
+  | ChooseSnippetType SnippetType
 
-makeEntry content index =
-  { content = content, index = index}
+type SnippetType
+  = PlainText
+  | StickyNote
+  | Markdown
+  | SoundCloud
+
+makeEntry : Model -> Snippet
+makeEntry {field, currentIndex, currentSnippetType} =
+  { content = field, index = currentIndex, snippetType = currentSnippetType}
 
 update action model =
   case action of
@@ -31,22 +44,46 @@ update action model =
       { model | field = field }
     AddEntry ->
       { model
-        | entries = model.entries ++ [makeEntry model.field model.currentIndex]
+        | entries = model.entries ++ [makeEntry model]
         , currentIndex = model.currentIndex + 1
       }
     DeleteEntry index ->
       { model | entries = List.filter (\t -> t.index /= index) model.entries }
+    ChooseSnippetType snippetType ->
+      { model |  currentSnippetType = snippetType }
+
+renderSnippetType snippet =
+  case snippet.snippetType of
+    PlainText ->
+      "this is plaintext"
+    StickyNote ->
+      "this is sticky note"
+    Markdown ->
+      "this is markdown"
+    SoundCloud ->
+      "this is sound cloud"
 
 renderEntry : Snippet -> Html
-renderEntry {content, index} =
+renderEntry snippet =
   div []
-  [ text content
-  , text (toString index)
-  , button [ onClick mainMailbox.address (DeleteEntry index) ] []
+  [ text snippet.content
+  , text (renderSnippetType snippet)
+  , button [ onClick mainMailbox.address (DeleteEntry snippet.index) ] []
   ]
 
 sendMessage a =
   Signal.message mainMailbox.address (UpdateField a)
+
+doChooseSnippetType a =
+  let
+    snippetType = case a of
+      "PlainText" -> PlainText
+      "Sticky note" -> StickyNote
+      "Markdown" -> Markdown
+      "SoundCloud" -> SoundCloud
+      _ -> PlainText
+  in
+    Signal.message mainMailbox.address (ChooseSnippetType snippetType)
 
 view : Model -> Html
 view model =
@@ -59,12 +96,17 @@ view model =
       ] []
     , button
       [ onClick mainMailbox.address AddEntry ] []
-    , text model.field
+    , select [on "change" targetValue doChooseSnippetType ]
+      [ option [] [ text "Plain text"]
+      , option [] [ text "Sticky note"]
+      , option [] [ text "Markdown"]
+      , option [] [ text "SoundCloud"]
+      ]
     , div [] (List.map renderEntry model.entries)
     ]
 
 emptyModel =
-  { field = "", entries = [], currentIndex = 0 }
+  { field = "", entries = [], currentIndex = 0, currentSnippetType = PlainText }
 
 mainMailbox = Signal.mailbox NoOp
 modelSignal = Signal.foldp update emptyModel mainMailbox.signal
