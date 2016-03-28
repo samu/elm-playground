5df574a75f8ac271ae72e78f551d814675cbd7ca
Samuel Mueller <mueller.samu@gmail.com>
2016-03-28 13:47:21 +0200

    v1

--- main.elm
+++ main.elm
@@ -4,9 +4,6 @@ import Html.Events exposing (..)
 import Graphics.Element exposing (..)
 import Signal exposing (..)
 
-main =
-  Signal.map view modelSignal
-
 type alias Model =
   { field : String
   , entries : List Snippet
@@ -18,17 +15,11 @@ type alias Snippet =
   , index : Int
   }
 
-emptyModel =
-  { field = "", entries = [], currentIndex = 0 }
-
 type Action
   = NoOp
   | UpdateField String
   | AddEntry
   | DeleteEntry Int
-
-mainMailbox = Signal.mailbox NoOp
-modelSignal = Signal.foldp update emptyModel mainMailbox.signal
 
 makeEntry content index =
   { content = content, index = index}
@@ -46,6 +37,17 @@ update action model =
     DeleteEntry index ->
       { model | entries = List.filter (\t -> t.index /= index) model.entries }
 
+renderEntry : Snippet -> Html
+renderEntry {content, index} =
+  div []
+  [ text content
+  , text (toString index)
+  , button [ onClick mainMailbox.address (DeleteEntry index) ] []
+  ]
+
+sendMessage a =
+  Signal.message mainMailbox.address (UpdateField a)
+
 view : Model -> Html
 view model =
   let
@@ -61,13 +63,11 @@ view model =
     , div [] (List.map renderEntry model.entries)
     ]
 
-renderEntry : Snippet -> Html
-renderEntry {content, index} =
-  div []
-  [ text content
-  , text (toString index)
-  , button [ onClick mainMailbox.address (DeleteEntry index) ] []
-  ]
+emptyModel =
+  { field = "", entries = [], currentIndex = 0 }
 
-sendMessage a =
-  Signal.message mainMailbox.address (UpdateField a)
+mainMailbox = Signal.mailbox NoOp
+modelSignal = Signal.foldp update emptyModel mainMailbox.signal
+
+main =
+  Signal.map view modelSignal
