6ecca6c7c8f3f9634fe464c9df329fc8f060aa57
Samuel Mueller <mueller.samu@gmail.com>
2016-04-18 16:13:40 +0200

    enhance editing

--- Snippet/PlainText.elm
+++ Snippet/PlainText.elm
@@ -1,18 +1,21 @@
 module Snippet.PlainText where
 import Html exposing (..)
 import Html.Attributes exposing (value)
-import Html.Events exposing (onDoubleClick, onBlur, targetValue, on)
+import Html.Events exposing (onClick, onBlur, targetValue, on)
 import Snippet.Base exposing (Action(..), Content)
 import Signal
 
 renderContent address content isEdited =
   case isEdited of
     True ->
-      input
-        [ value content
-        , onBlur address (IsEditing False)
-        , on "input" targetValue (Update >> (Signal.message address)) ] []
-    False -> div [ onDoubleClick address (IsEditing True) ] [ text content ]
+      div []
+        [ input [ value content, on "input" targetValue (Update >> (Signal.message address)) ] []
+        , button [ onClick address (IsEditing False) ] [ text "save" ] ]
+    False ->
+      div []
+        [ text content
+        , button [ onClick address (IsEditing True) ] [ text "edit" ]
+        ]
 
 render : Signal.Address Action -> { a | content : Content, id : Int, isEdited : Bool } -> Html
 render address {content, id, isEdited} =
