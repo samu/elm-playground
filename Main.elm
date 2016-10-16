52bb50d513d096871bfe7e91a56fd472c53e6786
Samuel Mueller <mueller.samu@gmail.com>
2016-10-16 12:33:05 +0200

    add resize subscription

--- Main.elm
+++ Main.elm
@@ -1,20 +1,37 @@
 import Html exposing (Html, Attribute, text, div, input)
-import Html.App exposing (beginnerProgram)
+import Html.App exposing (program)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
+import Collage exposing (collage, filled, rect)
+import Color exposing (rgb)
+import Element exposing (toHtml)
+import Window
 import String
 
 main =
-  beginnerProgram { model = "bla", view = view, update = update }
+  program { init = init, view = view, update = update, subscriptions = subscriptions }
 
 type alias Model = String
 
-type Message = Update Model
+init : (Model, Cmd Msg)
+init =
+  ("test", Cmd.none)
 
-update : Message -> Model -> Model
-update (Update string) oldString =
-  oldString
+subscriptions: Model -> Sub Msg
+subscriptions model =
+  Window.resizes (\{height, width} -> Resize height width)
 
-view : Model -> Html Message
+type Msg
+  = Update Model
+  | Resize Int Int
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg oldString =
+  case msg of
+    Update model -> (oldString, Cmd.none)
+    Resize height width -> (oldString, Cmd.none)
+
+view : Model -> Html Msg
 view string =
-  text (string)
+  collage 100 100 [filled (rgb 1 1 1) (rect 10 10)]
+  |> toHtml
---
+++ Todos.md
@@ -0,0 +1,11 @@
+# Pythagoras-Tree: Todos
+## UI
+* Drawing on canvas
+* Mouse events on canvas
+* "Drawing" with SVG elements
+* Mouse events with SVG elements
+
+## Math
+* Recursive data structure
+* Math functions (port them from JS project)
+* Testing
