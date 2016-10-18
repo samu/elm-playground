be57dc470f4c850ce28b6aa630557e68f9cd220e
Samuel Mueller <mueller.samu@gmail.com>
2016-10-18 20:57:00 +0200

    cleanup

--- Main.elm
+++ Main.elm
@@ -92,11 +92,23 @@ buildTree n form transformationMatrix previousMatrix =
     newMatrix = Transform.multiply previousMatrix transformationMatrix
     form' = groupTransform newMatrix [form]
   in
-    if n > 0
+    if n > 1
       then
         [form'] ++ buildTree (n-1) form transformationMatrix newMatrix
       else
         [form']
+
+getTransformationMatrix : Model -> Transform
+getTransformationMatrix model =
+  let
+    m1 = Transform.translation -20 20
+    m2 = Transform.rotation (degrees -22)
+    m3 = Transform.scale 0.8
+    m4 = Transform.translation 20 -20
+    m5 = Transform.translation 0 40
+  in
+    List.foldl Transform.multiply Transform.identity [m1, m2, m3, m4, m5]
+
 
 view : Model -> Html Msg
 view model =
@@ -105,34 +117,13 @@ view model =
     posX = screenCoordsToCollage mouseX width
     posY = screenCoordsToCollage mouseY height
 
-    m1 = Transform.translation -20 20
-    m2 = Transform.rotation (degrees -22)
-    m3 = Transform.scale 0.8
-    m4 = Transform.translation 20 -20
-    m5 = Transform.translation 0 40
-
-    mx4 = List.foldl Transform.multiply Transform.identity [m1, m2, m3, m4, m5]
-
-    -- mx5 = Transform.multiply mx4 mx4
-    -- mx6 = Transform.multiply mx5 mx4
-    -- mx7 = Transform.multiply mx6 mx4
-
-    -- form1 = drawRectangle (rgb 0 0 255) 40 40
     form1 = drawBaseShape model
 
-    -- form4 = groupTransform mx4 [form1]
-    -- form5 = groupTransform mx5 [form1]
-    -- form6 = groupTransform mx6 [form1]
-    -- form7 = groupTransform mx7 [form1]
+    mx4 = getTransformationMatrix model
 
     forms =
       [drawBackground model 0]
-      -- ++ [form1, form4, form5, form6, form7]
       ++ [form1]
       ++ buildTree 5 form1 mx4 Transform.identity
-
-    d = Debug.log "distance" (calculateDistance (-10, 0) (10, 0))
-    a = Debug.log "angle" (radians (calculateAngle (0, 1) (0, 0) (1, 0)))
-    degraas = Debug.log "degrees" (a * (180 / pi))
   in
     collage width height forms |> toHtml
